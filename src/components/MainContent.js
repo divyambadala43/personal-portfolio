e919647ec46633da100fd8e0e6c04ef442ead837
Divya <divyambadala43@gmail.com>
2022-03-26 16:24:31 +0530

    ADD: About Component

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./About";
 import Navbar from "./Navbar";
 
 
@@ -6,6 +7,7 @@ const MainContent = () => {
   return (
     <div className="main-content">
       <Navbar />
+      <About />
     </div>
   );
 };
