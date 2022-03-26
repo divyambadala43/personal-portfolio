4559e2f02a7d486dff4c1a6d5b4fb7e347bfda7b
Divya <divyambadala43@gmail.com>
2022-03-26 16:22:16 +0530

    ADD: MainContent and Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <main>
       <Sidebar />
+      <MainContent />
     </main>
   );
 }
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+
+const MainContent = () => {
+  return (
+    <div className="main-content">
+      <Navbar />
+    </div>
+  );
+};
+
+export default MainContent;
